479a4d2720b06d7d37bff786b33990a8d620c354
bloomtrader <nareshlife@outlook.com>
2020-09-20 01:34:49 +0530

    Added Request file

--- Node/play.js
+++ Node/play.js
@@ -1,1 +1,19 @@
-console.log('Hello World');
+var a = 10;
+var name = 'naresh';
+var srings = 'hellow aowld';
+var names = 'naresh';
+var b = 230;
+const add = (a, b) => {
+    console.log(a + b);
+};
+add(a, b);
+var arr = [
+    1,
+    2,
+    3,
+    4,
+    5
+];
+arr.forEach(num => {
+    console.log(num);
+});
